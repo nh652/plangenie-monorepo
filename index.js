0930568d3925500abee7c034eaa23e6b82ca0773
nh652 <87073819+nh652@users.noreply.github.com>
2025-07-24 13:57:22 +0530

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const axios = require("axios");
 const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
-const cors = require("cors");
+const cors = require("cors");  
 const path = require("path");
 
 console.log("🚀 Starting PlanGenie Server...");
